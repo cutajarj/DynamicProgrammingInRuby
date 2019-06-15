0a4ca597e676134850bbe58f8514432dc1e3bffc
cutajarj2 <james.cutajar@blip.pt>
2019-06-15 17:49:01 +0100

    String distance optimization

--- libs/strdistance/string_distance_opt.rb
+++ libs/strdistance/string_distance_opt.rb
@@ -2,27 +2,31 @@ class StringDistanceBottomUp
   def initialize(str_A, str_B)
     @str_A = str_A
     @str_B = str_B
-    @dist = Array.new(@str_A.length + 1) {Array.new(@str_B.length + 1, -1)}
+    @dist1 = Array.new(@str_B.length + 1, -1)
+    @dist2 = Array.new(@str_B.length + 1, -1)
     for a in 0..@str_A.length
       for b in 0..@str_B.length
         if a == 0
-          @dist[a][b] = b
+          @dist2[b] = b
         elsif b == 0
-          @dist[a][b] = a
+          @dist2[b] = a
         else
           replace_cost = @str_A[a - 1] == @str_B[b - 1] ? 0 : 1
-          cost_delete = @dist[a - 1][b] + 1
-          cost_insert = @dist[a][b - 1] + 1
-          cost_replace = @dist[a - 1][b - 1] + replace_cost
-          @dist[a][b] = [cost_delete, cost_insert, cost_replace].min
+          cost_delete = @dist1[b] + 1
+          cost_insert = @dist2[b - 1] + 1
+          cost_replace = @dist1[b - 1] + replace_cost
+          @dist2[b] = [cost_delete, cost_insert, cost_replace].min
         end
       end
-      puts(@dist[a].join(","))
+      puts(@dist2.join(","))
+      temp = @dist1
+      @dist1 = @dist2
+      @dist2 = temp
     end
   end
 
   def distance()
-    @dist[@str_A.length][@str_B.length]
+    @dist1[@str_B.length]
   end
 end
 
