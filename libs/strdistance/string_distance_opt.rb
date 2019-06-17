12c2e9621044a726131d27adad9f9f55bdfed830
cutajarj2 <james.cutajar@blip.pt>
2019-06-17 09:52:51 +0100

    renaming variables to match slides

--- libs/strdistance/string_distance_opt.rb
+++ libs/strdistance/string_distance_opt.rb
@@ -2,31 +2,31 @@ class StringDistanceBottomUp
   def initialize(str_A, str_B)
     @str_A = str_A
     @str_B = str_B
-    @dist1 = Array.new(@str_B.length + 1, -1)
-    @dist2 = Array.new(@str_B.length + 1, -1)
+    @dist_read = Array.new(@str_B.length + 1, -1)
+    @dist_write = Array.new(@str_B.length + 1, -1)
     for a in 0..@str_A.length
       for b in 0..@str_B.length
         if a == 0
-          @dist2[b] = b
+          @dist_write[b] = b
         elsif b == 0
-          @dist2[b] = a
+          @dist_write[b] = a
         else
           replace_cost = @str_A[a - 1] == @str_B[b - 1] ? 0 : 1
-          cost_delete = @dist1[b] + 1
-          cost_insert = @dist2[b - 1] + 1
-          cost_replace = @dist1[b - 1] + replace_cost
-          @dist2[b] = [cost_delete, cost_insert, cost_replace].min
+          cost_delete = @dist_read[b] + 1
+          cost_insert = @dist_write[b - 1] + 1
+          cost_replace = @dist_read[b - 1] + replace_cost
+          @dist_write[b] = [cost_delete, cost_insert, cost_replace].min
         end
       end
-      puts(@dist2.join(","))
-      temp = @dist1
-      @dist1 = @dist2
-      @dist2 = temp
+      puts(@dist_write.join(","))
+      temp = @dist_read
+      @dist_read = @dist_write
+      @dist_write = temp
     end
   end
 
   def distance()
-    @dist1[@str_B.length]
+    @dist_read[@str_B.length]
   end
 end
 
