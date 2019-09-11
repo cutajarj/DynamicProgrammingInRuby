21aa09efa7f41046576a2663aa6098e0ac6de7bf
cutajarj <james.cutajar@gmail.com>
2019-09-11 17:39:09 +0100

    Fixing final exercise test and adding readme

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # DynamicProgrammingInRuby
+
+Dynamic programming problems and solutions in Ruby
+
+The udemy course for this repo can be found at:
+
+TODO (Still working on it)
+
+Please do get in touch if you find any suggestions/improvements!
+
+Follow me on https://twitter.com/cutajarj
+
+And checkout my blog at: www.cutajarjames.com
+
+Here is funky robot:
+
+```
+           ___
+          |_|_|
+          |_|_|              _____
+          |_|_|     ____    |*_*_*|
+ _______   _\__\___/ __ \____|_|_   _______
+/ ____  |=|      \  <_+>  /      |=|  ____ \
+~|    |\|=|======\\______//======|=|/|    |~
+ |_   |    \      |      |      /    |    |
+  \==-|     \     |  2D  |     /     |----|~~/
+  |   |      |    |      |    |      |____/~/
+  |   |       \____\____/____/      /    / /
+  |   |         {----------}       /____/ /
+  |___|        /~~~~~~~~~~~~\     |_/~|_|/
+   \_/        |/~~~~~||~~~~~\|     /__|\
+   | |         |    ||||    |     (/|| \)
+   | |        /     |  |     \       \\
+   |_|        |     |  |     |
+              |_____|  |_____|
+              (_____)  (_____)
+              |     |  |     |
+              |     |  |     |
+              |/~~~\|  |/~~~\|
+              /|___|\  /|___|\
+             <_______><_______>
+```
+
--- test/full_bus_tour_test.rb
+++ test/full_bus_tour_test.rb
@@ -1,46 +1,47 @@
-require_relative "../libs/exercise/full_bus_tour"
+require_relative "../libs/exercise/solution/full_bus_tour_rec"
 require "test/unit"
+require "minitest/autorun"
 
-class TestFullBusTour < Test::Unit::TestCase
+class TestFullBusTour < Minitest::Test
 
   def test_simple_bus_full
     full_bus_tour = FullBusTour.new([3, 34, 4, 12, 5, 2], 9)
-    assert_true(full_bus_tour.fits_exactly, "[3, 34, 4, 12, 5, 2] with a bus capacity of 9 should fit with [4, 5]")
+    assert(full_bus_tour.fits_exactly, "[3, 34, 4, 12, 5, 2] with a bus capacity of 9 should fit with [4, 5]")
   end
 
   def test_simple_bus_full_again
     full_bus_tour = FullBusTour.new([3, 34, 4, 12, 5, 2], 10)
-    assert_true(full_bus_tour.fits_exactly, "[3, 34, 4, 12, 5, 2] with a bus capacity of 10 should fit with [3, 5, 2]")
+    assert(full_bus_tour.fits_exactly, "[3, 34, 4, 12, 5, 2] with a bus capacity of 10 should fit with [3, 5, 2]")
   end
 
   def test_small_bus_non_full
     full_bus_tour = FullBusTour.new([3, 34, 4, 12, 5, 2], 1)
-    assert_false(full_bus_tour.fits_exactly, "{3, 34, 4, 12, 5, 2} with a bus capacity of 1 should not fit")
+    assert(!full_bus_tour.fits_exactly, "{3, 34, 4, 12, 5, 2} with a bus capacity of 1 should not fit")
   end
 
   def test_ones_twos_bus_non_full
     full_bus_tour = FullBusTour.new([1, 2, 5, 8, 7, 12], 4)
-    assert_false(full_bus_tour.fits_exactly, "[1, 2, 5, 8, 7, 12] with a bus capacity of 4 should not fit")
+    assert(!full_bus_tour.fits_exactly, "[1, 2, 5, 8, 7, 12] with a bus capacity of 4 should not fit")
   end
 
   def test_even_only_bus_non_full
     full_bus_tour = FullBusTour.new([2, 4, 2, 8, 6, 4, 10, 6], 21)
-    assert_false(full_bus_tour.fits_exactly, "[2, 4, 2, 8, 6, 4, 10, 6] with a bus capacity of 21 should not fit")
+    assert(!full_bus_tour.fits_exactly, "[2, 4, 2, 8, 6, 4, 10, 6] with a bus capacity of 21 should not fit")
   end
 
   def test_odd_only_bus_full
     full_bus_tour = FullBusTour.new([1, 3, 5, 1, 7, 11, 9, 9], 21)
-    assert_true(full_bus_tour.fits_exactly, "[1, 3, 5, 1, 7, 11, 9, 9] with a bus capacity of 21 should fit with [11, 9, 1]")
+    assert(full_bus_tour.fits_exactly, "[1, 3, 5, 1, 7, 11, 9, 9] with a bus capacity of 21 should fit with [11, 9, 1]")
   end
 
   def test_big_bus_full
     full_bus_tour = FullBusTour.new([1, 3, 5, 4, 7, 11, 9, 9, 21, 7, 4, 21, 13, 8, 2, 5, 1, 18], 107)
-    assert_true(full_bus_tour.fits_exactly, "[1, 3, 5, 4, 7, 11, 9, 9, 21, 7, 4, 21, 13, 8, 2, 5, 1, 18] with a bus capacity of 107 should fit")
+    assert(full_bus_tour.fits_exactly, "[1, 3, 5, 4, 7, 11, 9, 9, 21, 7, 4, 21, 13, 8, 2, 5, 1, 18] with a bus capacity of 107 should fit")
   end
 
-  def testz_perf_test
+  def test_z_perf_test
     full_bus_tour = FullBusTour.new([1] * 500, 500)
-    assert_true(full_bus_tour.fits_exactly, "Performance test is failing. Improve your algorithm!")
+    assert(full_bus_tour.fits_exactly, "Performance test is failing. Improve your algorithm!")
   end
 
 end
